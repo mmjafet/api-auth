b76ff9b6aa65ff86d15ecb2c91cb8cc09ae29c2f
jafet <91506409+mmjafet@users.noreply.github.com>
2025-02-28 14:18:07 -0600

    Implement user authentication with fetch and error handling in authService

--- Service/authService.js
+++ Service/authService.js
@@ -1,8 +1,27 @@
 const login = async (username,password)=>{
+    const URL_USER = 'http://localhost:3001/users';
     try {
-        
+        const respose = await fetch(URL_USER + username );
+        if (respose.status === 200) {
+            const user = await respose.json();
+            if (user.username === username && user.password === password) {
+                return {
+                    token:"token-falso"+username.id
+                }
+            }else{
+                return {
+                    status:403,
+                    message:"Usuario o contraseña incorrecta"
+                }
+            }
+              
+        }
     }
     catch (error) {
+        return {
+            status:500,
+            message: error.message
+        }
         
     }
 
