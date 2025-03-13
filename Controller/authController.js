843215e520536913e860cfce318ed616225fcd78
jafet <91506409+mmjafet@users.noreply.github.com>
2025-03-12 23:39:37 -0600

    Refactor loginController to handle login response and error management

--- Controller/authController.js
+++ Controller/authController.js
@@ -3,7 +3,14 @@ const { login, signup } = require("../Service/authService");
 const loginController = async (req, res) => {
     const { username, password } = req.body;
     //console.log(username, password);
- 
+    try {
+        const respuesta = await login(username, password);
+        
+        if (respuesta.status === 200) {
+            res.status(respuesta.status).json(respuesta);
+        } else {
+            res.status(500).json({ message: "Error interno del servidor 2892198" });
+        }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
